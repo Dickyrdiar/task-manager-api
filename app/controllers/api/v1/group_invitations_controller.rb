f7e3f4e256eaa26c9baf46e1d953702912d9a49e
g-plex7 <co.write@yahoo.com>
2021-03-31 21:10:16 +0700

    create group invite controller

--- app/controllers/api/v1/group_invitations_controller.rb
+++ app/controllers/api/v1/group_invitations_controller.rb
@@ -1,10 +1,28 @@
 class Api::V1::GroupInvitationsController < ApplicationController
     def index
-        @groupInvitations = Groupinvitation.all 
+        @groupinvitations = Groupinvitation.all 
         render json: @groupinvitations
     end 
 
     def create
-          
+        @groupinvitation = Groupinvitation.new(groupinvitation_params.merge(user: current_user))
+
+        if @groupinvitation.save
+            if @groupinvitation.recipient != nil 
+                @user = User.find(params[:user_id])
+                GroupMailer.existing_user_invite(@groupinvitation).deliver
+                @groupinvitation.recipient.group.push(@groupinvitation.group)
+                render json: @groupinvitation, status: :ok
+            else 
+                # invitemailer 
+        else 
+            rendr json: { message: 'invite failed ' }, status: :failed
+        end 
+    end 
+
+    private  
+
+    def groupinvitation_params
+        params.require(:groupinvitation).permit(:group_id, :sender_id, :email, :username)
     end 
 end
---
+++ app/mailers/group_mailer.rb
@@ -0,0 +1,2 @@
+class GroupMailer < ApplicationMailer
+end
