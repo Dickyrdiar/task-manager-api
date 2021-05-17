351251a985c7fea03f65fe6a6fa9b5bcbcf4549e
dicky ardiar <dickyardiar1@gmail.com>
2021-05-17 10:49:31 +0700

    adding forest admin panel

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,13 +4,18 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+null_regex = Regexp.new(/\Anull\z/)
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    hostnames = [null_regex, 'localhost:4200', 'app.forestadmin.com', 'localhost:3001']
+    hostnames += ENV['CORS_ORIGINS'].split(',') if ENV['CORS_ORIGINS']
+
+    origins hostnames
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: %w[pages_count Content-Disposition],
+      credentials: true
+  end
+end
