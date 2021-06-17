40024b28be9e02d2297bed09ea09b3c84b35a060
dicky ardiar <dickyardiar1@gmail.com>
2021-06-17 22:24:48 +0700

    add policy permission

--- app/policies/message_policy.rb
+++ app/policies/message_policy.rb
@@ -1,2 +1,37 @@
 class MessagePolicy < ApplicationPolicy
+    attr_reader :user, :record
+
+    def initialize(user, record)
+        @user = user 
+        @record = record
+    end 
+
+    def index?
+        return true if @user.is_owner? 
+        return true if @user.is_members? 
+    end 
+
+    def show? 
+        true
+    end 
+
+    def create
+        index?
+    end 
+
+    def new? 
+       create? 
+    end 
+
+    def update? 
+        show? 
+    end 
+
+    def edit? 
+        show? 
+    end 
+
+    def destroy? 
+       show?
+    end 
 end                                                                                                                                                                                                                                                               
--- app/policies/project_invite_policy.rb
+++ app/policies/project_invite_policy.rb
@@ -1,2 +1,24 @@
 class ProjectInvitePolicy < ApplicationPolicy
+    attr_reader :user, :record 
+
+    def initialize(user, record)
+        @user = user 
+        @record = record
+    end 
+
+    def index
+        return true if @user.is_owner? 
+    end 
+
+    def show 
+        index?
+    end 
+
+    def new? 
+        create? 
+    end 
+
+    def create?
+        return true if @user.is_owner? 
+    end 
 end 
