8573125dce08de52511b30cc10b11f47b7e8f7c4
dicky ardiar <dickyardiar1@gmail.com>
2021-06-22 20:27:51 +0700

    fix project invites email

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,5 +1,6 @@
 class InviteMailer < ApplicationMailer
   def user_invite(invitation)
+    # @email = email
     # @url = url
     @invitation = invitation
     mail( to: @email , subject: "welcome to Teamwork")
