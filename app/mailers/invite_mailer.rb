614e1eb36680b557b46bb15ee8827063ec0a98f3
dicky ardiar <dickyardiar1@gmail.com>
2021-06-22 20:04:56 +0700

    fix auto send email

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,6 +1,7 @@
 class InviteMailer < ApplicationMailer
-  def user_invite(group_id, email)
-    @group = Group.find(group_id)
-    mail to: email, subject: "#{@invitation.email} send your link"
+  def user_invite(invitation)
+    # @url = url
+    @invitation = invitation
+    mail( to: @email , subject: "welcome to Teamwork")
   end  
 end
