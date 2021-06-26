234020fa5c02e9260c0aabec60b92cc20941bca9
dicky ardiar <dickyardiar1@gmail.com>
2021-06-26 19:56:59 +0700

    direct_message connecttion

--- db/migrate/20210608125214_create_direct_messages.rb
+++ db/migrate/20210608125214_create_direct_messages.rb
@@ -1,7 +1,8 @@
 class CreateDirectMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :direct_messages do |t|
-      t.text :direct_message
+      t.text :text 
+      t.string :image
 
       t.timestamps
     end
